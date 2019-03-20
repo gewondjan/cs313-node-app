f3de3019c0f3066211b971dac1ff1a8f2219e43a
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-19 20:27:34 -0600

    adding to heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({connectionString: connectionString});
+
+var stmt = "SELECT * FROM skills";
+
+pool.query(stmt, function(error, result) {
+    if (error) {
+        console.log("there was an error");
+    }
+
+    console.log(result.rows);
+
+});
