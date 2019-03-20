03622b744723ad43054e969a400a0febbda44ebc
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-19 21:38:14 -0600

    adding the files

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+//Need to figure out how to serve static pages and access views.
+app.use(express.static('views'), path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
 
