ab88e3ae04d397f0b61e131e6c97b0aff24d27ab
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-19 21:40:40 -0600

    adding to heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//TODO figure out why I'm getting the error (probably start with the node getting started repo and see what I am doing differently)
+
 //Need to figure out how to serve static pages and access views.
 app.use(express.static('views'), path.join(__dirname, 'views'));
 
