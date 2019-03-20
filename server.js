5ed767a2a262a52304f3bfbe85ab7beb0f5dac13
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-20 13:08:37 -0600

    adding to heroku

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const port = process.env.PORT|| 5432;
 
-//TODO figure out why I'm getting the error (probably start with the node getting started repo and see what I am doing differently)
-
-//Need to figure out how to serve static pages and access views.
 app.use('/views', express.static(path.join(__dirname, 'views')));
+
+//Set up the view engine
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
 
-    res.send('WHat up');
+    res.render('/views/index');
 
 });
 
