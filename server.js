9cb5ce67fb8eef07b4c44664d914fbaba029414c
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-20 13:33:57 -0600

    adding to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
 
-    res.render('./index', { body: './content/home'});
+    res.render('./index', { contentForBody: './content/home'});
 
 });
 
