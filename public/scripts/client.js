be28779fa12645fc342c9bdbb9b422926bf55ecf
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-20 20:27:44 -0600

    adding to heroku

---
+++ public/scripts/client.js
@@ -0,0 +1,3 @@
+function test() {
+    $('#homeHeader').html('This is different');
+}
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT|| 5432;
+const port = process.env.PORT;
 
-app.use('/views', express.static(path.join(__dirname, 'views')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 //Set up the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public', 'views'));
 
 app.get('/', (req, res) => {
+    res.render('./index', {contentForBody: 'home'});
+});
 
-    res.render('./index', { contentForBody: 'home'});
+app.get('/setUp', (req, res) => {
+    res.render('./index', {contentForBody: 'setup'});
 
 });
 
-app.listen(port, () => {});
+
+app.listen(port, () => console.log('Running on port ' + port));
 
